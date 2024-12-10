79aa6fecbc6defde42f89be06767fc3ed8f48f1b
BriceNogue <bricenogue@gmail.com>
2024-12-10 09:29:03 +0100

    First commit...

--- src/main/java/org/sebsy/demo/escaperooms/bll/Treasure1Service.java
+++ src/main/java/org/sebsy/demo/escaperooms/bll/Treasure1Service.java
@@ -1,5 +1,11 @@
 package org.sebsy.demo.escaperooms.bll;
 
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+@Service
+@Profile("trap")
 public class Treasure1Service implements TreasureService {
 
     public String ouvrir() {
